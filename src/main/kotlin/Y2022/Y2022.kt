5195a7632c5418862470af910d04534881bdf873
jeff <jmcda001@ucr.edu>
2022-12-05 20:19:05 -0800

    completing day 3

--- src/main/kotlin/Y2022/Y2022.kt
+++ src/main/kotlin/Y2022/Y2022.kt
@@ -2,12 +2,14 @@ package main.kotlin.Y2022
 
 import common.IncompleteDay
 import main.kotlin.Y2022.Day4.Day4
+import main.kotlin.Y2022.day3.Day3
 import main.kotlin.Y2022.days.D5
 import main.kotlin.common.Year
 
 class Y2022 : Year {
     override fun run(day: Int, part: Int, input: List<String>): String {
         val completedDays = mapOf(
+            3 to Day3(),
             4 to Day4(),
             5 to D5(),
         )
--- src/main/kotlin/Y2022/day3/Day3.kt
+++ src/main/kotlin/Y2022/day3/Day3.kt
@@ -2,7 +2,22 @@ package main.kotlin.Y2022.day3
 
 import common.Day
 class Day3 : Day {
+    private fun Char.priority() = if (isUpperCase()) { code - 38 } else { code - 96 }
+
     override fun execute(input: List<String>, part: Int): String {
-        TODO("Not yet implemented")
+        return if (part == 1) {
+            input.sumOf { rucksack ->
+                val compartment1 = rucksack.take(rucksack.length / 2).toSet()
+                val compartment2 = rucksack.takeLast(rucksack.length / 2).toSet()
+                compartment1.intersect(compartment2).first().priority()
+            }.toString()
+        } else {
+            input.chunked(3).sumOf {
+                val elf1 = it[0].toSet()
+                val elf2 = it[1].toSet()
+                val elf3 = it[2].toSet()
+                elf1.intersect(elf2).intersect(elf3).first().priority()
+            }.toString()
+        }
     }
 }
